bf06c0a7ac1d9bb052486ea373fc45885863af49
Ayush Vikram Singh <72918818+ayushsingh2204@users.noreply.github.com>
2020-10-19 16:45:10 +0530

    Update color.js

--- color.js
+++ color.js
@@ -1,17 +1,20 @@
 $('document').ready(()=>{
     $('#b1').click(()=>{
+        $("#b1").css("background-color", "red");
         $('.red').show();
         $('.orange').hide();
         $('.yellow').hide();
     });
 
     $('#b2').click(()=>{
+        $("#b2").css("background-color", "orange");
         $('.orange').show();
         $('.red').hide();
         $('.yellow').hide();
     });
 
     $('#b3').click(()=>{
+        $("#b3").css("background-color", "yellow");
         $('.yellow').show();
         $('.red').hide();
         $('.orange').hide();
